b379bebe266ebf29003c79b2fff2f8af99ace937
HaiNam362 <nguyenhainam362@gmail.com>
2025-07-30 11:20:19 +0700

    Fixbug

--- app/layout.js
+++ app/layout.js
@@ -3,21 +3,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// const geistSans = Geist({
-//     variable: "--font-geist-sans",
-//     subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//     variable: "--font-geist-mono",
-//     subsets: ["latin"],
-// });
-
-// export const metadata = {
-//     title: "Profile Nguyễn Hải Nam",
-//     description: "Profile Nguyễn Hải Nam",
-// };
-
 export default function RootLayout({ children }) {
     return (
         <html>
